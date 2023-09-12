210de9948546c146ac54a0cb8108e38690457ae7
Muhammadfaridrizki <Muhammadfaridrizki@gmail.com>
2023-09-12 18:38:07 +0700

    first commit

---
+++ latihan2.php
@@ -0,0 +1,13 @@
+<?php
+$nim = 'c030322117';
+$nama = 'Muhammad Farid Rizki';
+$alamat = 'Jl. Kelayan Besar ll';
+$nilai = 80;
+
+echo "NIM : ", $nim, "<br>";
+
+echo "Nama : ", $nama, "<br>";
+echo "Alamat : ", $alamat, "<br>";
+
+echo "Nilai : $nilai";
+?>
